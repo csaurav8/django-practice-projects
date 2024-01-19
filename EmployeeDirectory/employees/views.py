5f0aa3d0ec491a376b02cefe3be75a06c9e9f20a
csaurav8 <official.csaurav8@gmail.com>
2024-01-20 00:06:16 +0530

    added the functionality to view the details of employee

---
+++ EmployeeDirectory/employees/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Employee
+
+# Create your views here.
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        "employee": employee
+    }
+    return render(request, "employee_detail.html", context)
+    
+    # try:
+    #     employee = Employee.objects.get(id=pk)
+    # except:
+    #     raise Http404
+
