3984ec9ec2deaac2c70b218ffb9449e5977cca20
csaurav8 <official.csaurav8@gmail.com>
2024-01-20 07:20:27 +0530

    initialized todo project

--- Todo/todo_main/urls.py
+++ Todo/todo_main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for todo_main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,11 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
